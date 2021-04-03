b6389d1810f14b8651392adf3911f6e8b48abf0f
muattarmehdi <73297804+muattarmehdi@users.noreply.github.com>
2021-04-04 02:15:47 +0500

    Add files via upload

---
+++ Driveclass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.prototype.mid.term.project;
+
+/**
+ *
+ * @author inn
+ */
+public class Driveclass {
+    public static void main(String[] args)
+    {
+        FirstPage f = new FirstPage();
+        f.setVisible(true);
+    
+    }
+    
+}
